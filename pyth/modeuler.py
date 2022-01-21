0960f6b7a86e4ec1f6014c1fca9bdaed13172d09
michaelnaps <napoli.38@osu.edu>
2022-01-21 11:12:31 -0500

    created python 2 link statespace, only compilation testing

--- pyth/modeuler.py
+++ pyth/modeuler.py
@@ -5,10 +5,8 @@
 # Purpose: To calculate the states of a given statespace
 #   over a predetermined prediction horizon.
 
-def statespace(q, u, c, m, L, jNum):
-    return [i for i in range(2*jNum)];
-
-def modeuler(P, dt, q0, u, c, m, L, jNum):
+def modeuler(P, dt, q0, u, c, m, L, statespace):
+    jNum = len(u);
     Pm = P*10;  dtm = dt/10;
     q = [[0 for j in range(2*jNum)] for i in range(P+1)];
     qm = [[0 for j in range(2*jNum)] for i in range(Pm+1)];
@@ -19,12 +17,10 @@ def modeuler(P, dt, q0, u, c, m, L, jNum):
         dq1 = statespace(qm[i], u, c, m, L, jNum);
         qeu = [(qm[i][j] + dq1[j]*dtm) for j in range(2*jNum)];
         qeu = [(i*j) for j in range(2*jNum)];
-        dq2 = statespace(qeu, u, c, m, L, jNum);
+        dq2 = statespace(qeu, u, c, m, L);
         qm[i+1] = [(qm[i][j] + 1/2*(dq1[j] + dq2[j])*dtm) for j in range(2*jNum)];
 
         if (i % 10 == 0):
             q[int(i/10+1)] = qm[i+1];
 
     return q;
-
-print(modeuler(4, 0.025, [1,2,3,4,5,6], 1, 1, 1, 1, 3));
