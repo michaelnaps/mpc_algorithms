d3616fa0da988b21c77051864e6bf19233546c7a
Michael Napoli <napoli.38@osu.edu>
2022-01-25 14:45:52 -0500

    statespace of digit added, created by Professor Hereid

---
+++ digit/statespace_digit.m
@@ -0,0 +1,73 @@
+function [xdot] = compute_xdot(model, x, u)
+    % This computes the xdot = [dq;ddq] given a controller input `u` and
+    % states `x=[q;dq]`. 
+    
+       
+    % extract the state variables into x and dx
+    nx = model.Dimension;
+    q = x(1:nx);
+    dq = x(nx+1:end);
+        
+    % compute the mass matrix and drift vector (internal dynamics)
+    M = calcMassMatrix(model, q);
+    Fv = calcDriftVector(model, q, dq);
+    
+    
+        
+    %% NOTE: 
+    % here we assume no external input, and the joint torques are given by
+    % the input arguments `u`. Therefore no need to call the callback
+    % function. 
+    torque = model.Inputs.torque;
+    Gmap = feval(torque.Gmap.Name,q);
+    Gv   = Gmap*u;
+    
+    
+    ddq_free = M\(-Fv+Gv);
+    
+    %% holonomic constraints
+    h_cstr_name = fieldnames(model.HolonomicConstraints);
+    if ~isempty(h_cstr_name)           % if holonomic constraints are defined
+        h_cstr = struct2array(model.HolonomicConstraints);
+        n_cstr = length(h_cstr);
+        % determine the total dimension of the holonomic constraints
+        cdim = sum([h_cstr.Dimension]);
+        % initialize the Jacobian matrix
+        Je = zeros(cdim,nx);
+        Jedot = zeros(cdim,nx);
+        
+        idx = 1;
+        for i=1:n_cstr
+            cstr = h_cstr(i);
+            
+            % calculate the Jacobian
+            [Jh,dJh] = calcJacobian(cstr,q,dq);
+            cstr_indices = idx:idx+cstr.Dimension-1;
+                    
+            Je(cstr_indices,:) = Jh;
+            Jedot(cstr_indices,:) = dJh; 
+            idx = idx + cstr.Dimension;
+        end 
+    else
+        Je = [];
+        Jedot = [];        
+    end
+    
+    if isempty(Je)
+        ddq = ddq_free;
+    else
+        Xi = Je * (M \ transpose(Je));
+        P = eye(nx) - M\(transpose(Je)/Xi)*Je;
+        %         ddq = M\(-Fv+Gv+Je'*lambda);
+        ddq = P*ddq_free;
+        
+    end
+    
+   
+    
+    % the system dynamics
+    xdot = [dq; 
+        ddq];
+    
+end
+
