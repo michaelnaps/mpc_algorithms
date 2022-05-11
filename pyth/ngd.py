2af6c2bed849a6c184aef20eb4aa58aa8451efa9
michaelnaps <napoli.38@osu.edu>
2022-05-10 20:40:00 -0400

    implementing ngd algorithm, system diverging with bkl search

---
+++ pyth/ngd.py
@@ -0,0 +1,195 @@
+# Package: nno.py
+# Created by: Michael Napoli
+# Created on: 3/27/2022
+# Last modified on: 3/27/2022
+
+
+import numpy as np
+import math
+import time
+import pickle
+from modeuler import *
+
+
+def mpc_root(mpc_var, q0, u0, inputs, output=0):
+    # MPC variable declaration
+    N  = mpc_var.num_inputs;
+    P  = mpc_var.PH_length;
+    dt = mpc_var.time_step;
+    eps = mpc_var.appx_zero;
+    tspan = mpc_var.sim_time;
+
+    # simulation time variables
+    Nt = int(tspan/dt+1);
+    T = [i*dt for i in range(Nt)];
+
+    # state matrices declarations
+    qlist = [0 for i in range(Nt)];
+    qlist[0] = q0;
+
+    # return variables
+    ulist = [0 for i in range(Nt)];
+    Clist = [0 for i in range(Nt)];
+    nlist = [0 for i in range(Nt)];
+    brklist = [0 for i in range(Nt)];
+    t_run = [0 for i in range(Nt)];
+
+    ulist[0] = u0;
+
+    for i in range(1,Nt):
+        if output:  print("\nTime: %0.3f" % (T[i]));
+
+        t = time.time();
+        opt_results = newtons(mpc_var, qlist[i-1], ulist[i-1][:N], ulist[i-1], inputs, output);
+        elapsed = time.time() - t;
+
+        ulist[i]   = opt_results[0];
+        Clist[i]   = opt_results[1];
+        nlist[i]   = opt_results[2];
+        brklist[i] = opt_results[3];
+        t_run[i]   = 1000*elapsed;
+
+        if output:  print("Elapsed Time:\n              ", t_run[i]);
+
+        # inverse dynamics: lapm -> 3link (digit)
+
+        qlist[i] = modeuler(mpc_var, qlist[i-1], ulist[i][:N], inputs)[1][1];
+
+    return (T, qlist, ulist, Clist, nlist, brklist, t_run);
+
+def newtons(mpc_var, q0, u0, uinit, inputs, output=0):
+    # MPC constants initialization
+    P    = mpc_var.PH_length;
+    N    = mpc_var.num_inputs;
+    eps  = mpc_var.appx_zero;
+    imax = mpc_var.max_iter;
+    a    = mpc_var.step_coeff;
+
+    # loop variable setup
+    uc = uinit;
+    Cc = cost(mpc_var, q0, u0, uc, inputs);
+    un = uc;  Cn = Cc;
+
+    if output:
+        print("Opt. Start:");
+        print("Initial Cost: ", Cc);
+
+    count = 1;
+    brk = -2*np.isnan(Cc);
+    while (Cc > eps):
+        # calculate the gradient around the current input
+        g = gradient(mpc_var, q0, u0, uc, inputs);
+        gnorm = np.sqrt(np.sum([g[i]**2 for i in range(N)]));
+
+        # check if gradient-norm is an approx. of zero
+        if (gnorm < eps):
+            brk = 1;
+            break;
+
+        # calculate the next iteration of the input
+        un = alpha_bkl(g, Cc, mpc_var, q0, u0, uc, inputs)[0];
+
+        # simulate and calculate the new cost value
+        Cn = cost(mpc_var, q0, u0, un, inputs);
+        count += 1;  # iterate the loop counter
+
+        if (np.isnan(Cn)):
+            brk = -2;
+            break;
+
+        if output:
+            # print("Gradient:  ", g);
+            print("|g|:          ", gnorm);
+            print("New Cost:     ", Cn);
+            print("New Input: ");
+            for i in range(0, N*P, 2):
+                print("              ", un[i], un[i+1]);
+
+        # break conditions
+        if (count > imax):
+            brk = -1;
+            break;
+
+        # update loop variables
+        uc = un;  Cc = Cn;
+
+    return (un, Cn, count, brk);
+
+
+def cost(mpc_var, q0, u0, u, inputs):
+    # MPC constants
+    N  = mpc_var.num_ssvar;
+    Nu = mpc_var.num_inputs;
+    P  = mpc_var.PH_length;
+    Cq = mpc_var.cost_state;
+    Cu = mpc_var.cost_input;
+    Ccmp = mpc_var.cost_CMP;
+    qd = mpc_var.des_config;
+
+    # reshape input variable
+    uc = np.reshape(u, [P, Nu]);
+
+    # calculate change in input
+    du = [[0 for j in range(Nu)] for i in range(P)];
+    du[0] = [uc[0][i] - u0[i] for i in range(Nu)];
+    for i in range(1, P):
+        for j in range(Nu):
+            du[i][j] = uc[i][j] - uc[i-1][j];
+
+    # Cost of each input over the designated windows
+    # simulate over the prediction horizon and sum cost
+    q = [[0 for i in range(2*N)] for j in range(P+1)];
+    q[0] = q0;
+    for i in range(P):
+        q[i+1] = modeuler(mpc_var, q[i], uc[i], inputs)[1][-1];
+
+    C = [0 for i in range(P+1)];
+
+    for i in range(P+1):
+        C[i] = C[i] + Cq(qd, q[i]);  # state cost
+        if i != P:
+            C[i] = C[i] + Cu(uc[i], du[i], inputs);  # input costs
+            C[i] = C[i] + Ccmp(uc[i], inputs);  # CMP costs
+
+    return np.sum(C);
+
+
+def gradient(mpc_var, q, u0, u, inputs, rownum=1):
+    # variable setup
+    N = mpc_var.num_inputs*mpc_var.PH_length;
+    h = mpc_var.step_size;
+    g = [0 for i in range(N)];
+
+    for i in range(rownum-1, N):
+        un1 = [u[j] - (i==j)*h for j in range(N)];
+        up1 = [u[j] + (i==j)*h for j in range(N)];
+
+        Cn1 = cost(mpc_var, q, u0, un1, inputs);
+        Cp1 = cost(mpc_var, q, u0, up1, inputs);
+
+        g[i] = (Cp1 - Cn1)/(2*h);
+
+    return g;
+
+def alpha_bkl(g, C0, mpc_var, q0, u0, uc, inputs):
+    P = mpc_var.PH_length;
+    N = mpc_var.num_inputs;
+    w = mpc_var.bkl_shrink;
+    a = mpc_var.step_coeff;
+    abkl = a;
+
+    count = 0;
+    brk = -1;
+    while (count != 1000):
+        ubkl = [uc[i] - abkl*g[i] for i in range(P*N)];
+        Cbkl = cost(mpc_var, q0, u0, ubkl, inputs);
+        count += 1;
+
+        if (Cbkl < C0):
+            brk = 0;
+            break;
+
+        abkl *= w;
+
+    print("Alpha: ", abkl);
+    return (ubkl, Cbkl, abkl, count, brk);
---
+++ pyth/test/root_ngd.py
@@ -0,0 +1,79 @@
+import sys
+
+sys.path.insert(0, '/home/michaelnaps/prog/mpc_algorithms/pyth/.');
+sys.path.insert(0, 'models/.');
+
+import ngd
+from statespace_lapm import *
+import matplotlib.pyplot as plt
+
+def Cq(qd, q):
+    Cq = [
+        100*(qd[0] - q[0])**2 +  1*(qd[2] - q[2])**2,
+         10*(qd[1] - q[1])**2 +    (qd[3] - q[3])**2
+    ];
+
+    return np.sum(Cq);
+
+def Cu(u, du, inputs):
+    umax = inputs.input_bounds;
+
+    Cu = [
+        1e-5*(du[0])**2 - np.log(umax[0]**2 - u[0]**2) + np.log(umax[0]**2),
+        1e-5*(du[1])**2# - np.log(umax[1]**2 - u[1]**2) + np.log(umax[1]**2)
+    ];
+
+    return np.sum(Cu);
+
+def Ccmp(u, inputs):
+    dmax = inputs.CP_maxdistance;
+    g = inputs.gravity_acc;
+    m = inputs.joint_masses[0];
+
+    utip = m*g*dmax;
+
+    Ccmp = [
+        #-np.log(utip**2 - u[0]**2) + np.log(utip**2),
+        100*(u[0]/utip)**2,
+        0
+    ];
+
+    return np.sum(Ccmp);
+
+class MPCVariables:
+    def __init__(self):
+        self.sim_time    = 2;
+        self.model       = statespace_lapm;
+        self.cost_state  = Cq;
+        self.cost_input  = Cu;
+        self.cost_CMP    = Ccmp;
+        self.PH_length   = 4;
+        self.knot_length = 2;
+        self.time_step   = 0.025;
+        self.appx_zero   = 1e-6;
+        self.step_size   = 1e-3;
+        self.num_ssvar   = 2;
+        self.num_inputs  = 2;
+        self.des_config  = [0, 0, 0, 0];
+        self.max_iter    = 5;
+        self.step_coeff  = 10000;
+        self.bkl_shrink  = 0.9
+
+class InputVariables:
+    def __init__(self):
+        self.gravity_acc          = -9.81;
+        self.damping_coefficients = [0];
+        self.joint_masses         = [80];
+        self.link_lengths         = [2.0];
+        self.CP_maxdistance       = 0.1;
+        self.input_bounds         = [60, 150];
+
+inputs  = InputVariables();
+mpc_var = MPCVariables();
+
+q0 = [0-0.05, 0, 0, 0];
+u0 = [0 for i in range(mpc_var.num_inputs*mpc_var.PH_length)];
+
+mpc_results = ngd.mpc_root(mpc_var, q0, u0, inputs, 1);
+
+reportResults_lapm(inputs, mpc_var, mpc_results);
